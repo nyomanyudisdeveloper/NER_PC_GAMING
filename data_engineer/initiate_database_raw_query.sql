6768f0102faea60288c92075ec8c67562bd5e6a2
nyomanyudisdeveloper <nyomanyudisdeveloper@gmail.com>
2024-08-01 14:53:27 +0700

    data analyst

--- data_engineer/initiate_database_raw_query.sql
+++ data_engineer/initiate_database_raw_query.sql
@@ -7,12 +7,13 @@ CREATE TABLE discussion(
 );
 
 CREATE TABLE kata_tag_dict(
+	sentence VARCHAR(1000),
 	kata VARCHAR(100),
 	tag VARCHAR(50)
 );
 
 -- THis can only be run in PSQL Tool
--- \COPY kata_tag_dict FROM '/Users/nyomanyudis/Desktop/hactiv8/web scraping/NER_PC_GAMING/dataset/word_tag.csv' DELIMITER ',' CSV HEADER;
+-- \COPY kata_tag_dict FROM '/Users/nyomanyudis/Desktop/hactiv8/web scraping/NER_PC_GAMING/dataset/DatasetWithTagFinal.csv' DELIMITER ',' CSV HEADER;
 -- \COPY discussion FROM '/Users/nyomanyudis/Desktop/hactiv8/web scraping/NER_PC_GAMING/dataset/question.csv' DELIMITER ',' CSV HEADER;
 
 END;
